2b42f03fce8775c9ae2bbc094d8d219ff2f8fb46
Richard Janssen <richard.janssen@student.hu.nl>
2023-08-20 17:53:13 +0200

    Only add decorator when main.py is run

--- main.py
+++ main.py
@@ -2,18 +2,6 @@ import time
 import LightIntensityController
 import GroundHumidityController
 
-if(True): # Add Decorator
-    import GroundHumiditySensor
-    def GetGroundHumiditySensor_decorator(f):
-        def inner():
-            return_value = f()
-            print('GroundHumiditySensorValue is:', return_value)
-            return return_value
-        return inner
-
-    GroundHumiditySensor.GetSensorReading = GetGroundHumiditySensor_decorator(GroundHumiditySensor.GetSensorReading)
-    del GroundHumiditySensor
-
 def main():
     while(True):
         print("Update")
@@ -22,4 +10,16 @@ def main():
         time.sleep(1)
 
 if __name__ == "__main__":
+    if(True): # Add Decorator
+        import GroundHumiditySensor
+        def GetGroundHumiditySensor_decorator(f):
+            def inner():
+                return_value = f()
+                print('GroundHumiditySensorValue is:', return_value)
+                return return_value
+            return inner
+
+        GroundHumiditySensor.GetSensorReading = GetGroundHumiditySensor_decorator(GroundHumiditySensor.GetSensorReading)
+        del GroundHumiditySensor
+    
     main()
