5a2b2cb57752b349941a59d0c5fb8ac674d672f6
Richard Janssen <richard.janssen@student.hu.nl>
2023-08-20 17:48:27 +0200

    Moved decorator

--- main.py
+++ main.py
@@ -2,7 +2,17 @@ import time
 import LightIntensityController
 import GroundHumidityController
 
+if(True): # Add Decorator
+    import GroundHumiditySensor
 
+    def GetGroundHumiditySensor_decorator(f):
+        def inner():
+            return_value = f()
+            print('GroundHumiditySensorValue is:', return_value)
+            return return_value
+        return inner
+
+    GroundHumiditySensor.GetSensorReading = GetGroundHumiditySensor_decorator(GroundHumiditySensor.GetSensorReading)
 
 
 def main():
